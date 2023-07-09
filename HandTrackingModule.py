c8c6e759d3f9b2ce5dc4cfa42d9c3a9e408e27ab
tejas <teja77209986776@gmail.com>
2023-07-09 16:00:09 +0530

    first commit

---
+++ HandTrackingModule.py
@@ -0,0 +1,63 @@
+import  cv2
+import mediapipe as mp
+import  time
+
+# class creation
+class handDetector():
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5,modelComplexity=1,trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.modelComplex = modelComplexity
+        self.trackCon = trackCon
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,self.modelComplex,
+                                        self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils # it gives small dots onhands total 20 landmark points
+
+    def findHands(self,img,draw=True):
+        # Send rgb image to hands
+        imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB) # process the frame
+    #     print(results.multi_hand_landmarks)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+
+                if draw:
+                    #Draw dots and connect them
+                    self.mpDraw.draw_landmarks(img,handLms,
+                                                self.mpHands.HAND_CONNECTIONS)
+
+        return img
+
+    def findPosition(self,img, handNo=0, draw=True):
+
+
+        lmlist = []
+
+        # check wether any landmark was detected
+        if self.results.multi_hand_landmarks:
+            #Which hand are we talking about
+            myHand = self.results.multi_hand_landmarks[handNo]
+            # Get id number and landmark information
+            for id, lm in enumerate(myHand.landmark):
+                 # id will give id of landmark in exact index number
+                # height width and channel
+                h,w,c = img.shape
+                #find the position
+                cx,cy = int(lm.x*w), int(lm.y*h) #center
+                # print(id,cx,cy)
+                lmlist.append([id,cx,cy])
+
+                # Draw circle for 0th landmark
+                if draw:
+                    cv2.circle(img,(cx,cy), 5 , (255,0,255), cv2.FILLED)
+
+        return lmlist
+
+def main():
+   pass
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,87 @@
+import cv2
+import time
+import os
+import HandTrackingModule as htm
+from pynput.keyboard import Controller
+
+# Initialize hand detector
+detector = htm.handDetector(detectionCon=0.75)
+
+# Set up video capture
+cap = cv2.VideoCapture(0)
+wCam, hCam = 640, 480
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+# Load overlay images from the specified folder
+folderpath = "folderlist"
+myList = os.listdir(folderpath)
+
+overlayList = []
+for imPath in myList:
+    image = cv2.imread(os.path.join(folderpath, imPath))
+    if image is not None:
+        overlayList.append(image)
+    else:
+        print(f"Failed to load image: {imPath}")
+
+print(f"Number of loaded overlay images: {len(overlayList)}")
+
+tipsIds = [4, 8, 12, 16, 20]
+keyboard = Controller()
+fistClosed = False
+
+while True:
+    # Read video frame
+    success, img = cap.read()
+    if not success:
+        print("Failed to read frame from the video source.")
+        break
+
+    # Detect hands in the frame
+    img = detector.findHands(img)
+    imList = detector.findPosition(img, draw=False)
+    fingers = []
+
+    if len(imList) != 0:
+        # Check thumb finger
+        if imList[tipsIds[0]][1] < imList[tipsIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # Check other fingers
+        for id in range(1, 5):
+            if imList[tipsIds[id]][2] < imList[tipsIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        totalFingers = fingers.count(1)
+        print(f"Number of raised fingers: {totalFingers}")
+
+        if totalFingers <= len(overlayList):
+            # Overlay the corresponding image on top of the frame
+            h, w, c = overlayList[totalFingers - 1].shape
+            img[0:h, 0:w] = overlayList[totalFingers - 1]
+
+            # Check if the fist is closed
+            if totalFingers == 0:
+                if fistClosed:
+                    fistClosed = False
+                    keyboard.release(' ')
+            else:
+                if not fistClosed:
+                    fistClosed = True
+                    keyboard.press(' ')
+
+    # Display the modified frame
+    cv2.imshow("image", img)
+
+    # Exit the loop when 'q' is pressed
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# Release resources
+cap.release()
+cv2.destroyAllWindows()
